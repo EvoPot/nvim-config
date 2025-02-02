62b55694df96f4431d6bff6b0ff49e518e74de85
evopot <evolvedpot@gmail.com>
2025-02-02 13:54:28 +0300

    change barbar and nvim tree shortcuts

--- lua/evopot/remap.lua
+++ lua/evopot/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " ";
-vim.keymap.set('n','<C-1>', ":NvimTreeToggle<CR>")
-vim.keymap.set('n','<C-2>', ":NvimTreeFocus<CR>")
-vim.keymap.set('n','<A-1>', ":BufferPrevious<CR>")
-vim.keymap.set('n','<A-2>', ":BufferNext<CR>")
+vim.keymap.set('n','<A-1>', ":NvimTreeToggle<CR>")
+vim.keymap.set('n','<A-2>', ":NvimTreeFocus<CR>")
+vim.keymap.set('n','<A-h>', ":BufferPrevious<CR>")
+vim.keymap.set('n','<A-l>', ":BufferNext<CR>")
